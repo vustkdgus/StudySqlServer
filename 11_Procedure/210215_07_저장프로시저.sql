6d8a8ba42ff67e21dacbdd92eeebb9f2985da9b6
vustkdgus <vustkdgus@naver.com>
2021-02-15 17:26:28 +0900

    2월 15일 커밋

---
+++ 11_Procedure/210215_07_저장프로시저.sql
@@ -0,0 +1,21 @@
+use sqlDB;
+go
+
+create or alter procedure usp_iselse
+	@userName nvarchar(10)
+as
+	declare @birthYear int; --출생년도 저장 변수
+	select @birthYear = birthYear from userTbl
+	where userName = @userName;
+
+	if (@birthYear >= 1980)
+	begin
+		print '아직 젊군요...';
+	end
+	else
+	begin
+		print '나이가 지긋...';
+	end
+go
+
+exec usp_iselse '김경호'
---
+++ 11_Procedure/210215_11_사용자정의함수.sql
@@ -0,0 +1,12 @@
+--사용자 정의 함수 사용안한 것
+select
+	*,
+	(year(getdate()) - birthYear) + 1 as 나이
+	from userTbl;
+
+
+--사용자 정의 함수 사용
+SELECT *,
+		dbo.ufn_getAge(birthYear) as 나이,
+		dbo.ufn_getZodiac(birthYear) as 띠
+	from userTbl;

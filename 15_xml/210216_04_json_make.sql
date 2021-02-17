a9de759d98e3a78f7b2e3bba2dcd71eb2066bcc5
vustkdgus <vustkdgus@naver.com>
2021-02-17 09:07:09 +0900

    2월16일 커밋

---
+++ 15_xml/210216_04_json_make.sql
@@ -0,0 +1,6 @@
+select u.userID, u.userName, u.birthYear, u.addr, u.height , b.prodName, b.price
+  from userTbl as u
+inner join buyTbl as b
+	on u.userID = b.userID
+ where height > 170
+ for json auto;
